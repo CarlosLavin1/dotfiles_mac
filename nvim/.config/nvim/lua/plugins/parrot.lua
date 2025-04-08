1bdb509daacad24428acf655664b063fb952c61e
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-04-08 15:10:18 -0300

    added parrot nvim and some ghostty stuff

---
+++ nvim/.config/nvim/lua/plugins/parrot.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"frankroeder/parrot.nvim",
+		dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "folke/noice.nvim" },
+		-- optionally include "folke/noice.nvim" or "rcarriga/nvim-notify" for beautiful notifications
+		config = function()
+			require("parrot").setup({
+				-- Providers must be explicitly added to make them available.
+				providers = {
+					anthropic = {},
+					gemini = {
+						api_key = os.getenv("GEMINI_API_KEY"),
+					},
+					-- provide an empty list to make provider available (no API key required)
+					openai = {},
+				},
+			})
+		end,
+	},
+}
