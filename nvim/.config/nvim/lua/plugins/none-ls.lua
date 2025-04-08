8eee54c4cf7eceb7c1af969582ab47e7a6444ac3
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-04-08 15:55:08 -0300

    added autopairs plugin

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -11,8 +11,6 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.erb_lint,
-        require("none-ls.diagnostics.eslint"),
-        require("none-ls.code_actions.eslint"),
         null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.formatting.black
       },
