4f0cc0673ca4f86953477e1531234734090458b1
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-04-13 22:27:04 -0300

    updated rosepine plugin

--- nvim/.config/nvim/lua/plugins/rosepine.lua
+++ nvim/.config/nvim/lua/plugins/rosepine.lua
@@ -1,20 +1,20 @@
 return {
-    "rose-pine/neovim",
-	  name = "rose-pine",
-    extended_background_behind_borders = true,
-    disable_background = true,
-    disable_float_background = true,
-    enable = {
-      terminal = true
-    },
-    priority = 1000,
-	  config = function()
-		  vim.cmd("colorscheme rose-pine")
+	"rose-pine/neovim",
+	name = "rose-pine",
+	extended_background_behind_borders = true,
+	disable_background = true,
+	disable_float_background = true,
+	enable = {
+		terminal = true,
+	},
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme rose-pine")
 
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })          -- Main background
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })     -- Floating windows
-      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })      -- Gutter (for Git signs)
-      vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })          -- Line numbers
-      vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
-	  end
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Main background
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Floating windows
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" }) -- Gutter (for Git signs)
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" }) -- Line numbers
+		vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+	end,
 }
