3aaacb1bdb3ba87424c36f7591634a9c47213832
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-04-29 23:53:45 -0300

    added tailwindcss lsp to nvim config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -32,18 +32,30 @@ return {
                 capabilities = capabilities,
             })
             lspconfig.pyright.setup({
-                capabilities =capabilities
+                capabilities = capabilities,
             })
             lspconfig.clangd.setup({
-                capabilities =capabilities
+                capabilities = capabilities,
             })
             lspconfig.rust_analyzer.setup({
-                capabilities =capabilities
+                capabilities = capabilities,
             })
 
             vim.keymap.set("n", "<C-i>", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end,
+    },
+    -- tailwind plugin
+    {
+        "luckasRanarison/tailwind-tools.nvim",
+        name = "tailwind-tools",
+        build = ":UpdateRemotePlugins",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {},
     },
 }
